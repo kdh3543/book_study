c8856aef6207007d3490881abf180bb8d6ce048c
김동현 <rlaehdgus3543@naver.com>
2023-04-24 15:44:37 +0900

    4/24 study 배열 part

---
+++ JS_Basic/07/js/checklist.js
@@ -0,0 +1,35 @@
+let itemList = []
+let addBtn = document.querySelector('#add')
+addBtn.addEventListener('click', addList)
+
+function addList() {
+  let item = document.querySelector('#item').value
+  if (item != null) {
+    itemList.push(item)
+    document.querySelector('#item').value = ''
+    document.querySelector('#item').focus()
+  }
+  showList()
+}
+
+function showList() {
+  var list = '<ul>'
+  for (let i = 0; i < itemList.length; i++) {
+    list += '<li>' + itemList[i] + `<span class='close' id=${i}>X</span></li>`
+  }
+  list += '</ul>'
+
+  document.querySelector('#itemList').innerHTML = list
+  removeList()
+}
+
+function removeList() {
+  let remove = document.querySelectorAll('.close')
+
+  for (let i = 0; i < remove.length; i++) {
+    remove[i].addEventListener('click', removeList)
+  }
+  let id = this.getAttribute('id')
+  itemList.splice(id, 1)
+  showList()
+}
---
+++ JS_Basic/07/js/quiz-2.js
@@ -0,0 +1,4 @@
+let array = ['자존심', '후회를 이용', '새로운 삶']
+
+let num = Math.floor(Math.random() * array.length)
+document.write(`<p>&quot ${array[num]} &quot</p>`)
