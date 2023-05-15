8f218717780221a98bd3d98c35cf3ca777b6a803
김동현 <rlaehdgus3543@naver.com>
2023-05-15 17:09:32 +0900

    5/15 react study class component

--- react_basic/src/App.js
+++ react_basic/src/App.js
@@ -1,18 +1,57 @@
 import './App.css'
-import React from 'react'
+import React, { Component, useState } from 'react'
 import TOC from './components/TOC'
 import Content from './components/Content'
 import Subject from './components/Subject'
 
-function App() {
-  return (
-    <div className="App">
-      <Subject title="WEB" sub="world wide"></Subject>
-      <Subject title="react" sub="facebook"></Subject>
-      <TOC />
-      <Content />
-    </div>
-  )
+// function App() {
+//   const title = useState('WEB')
+//   const subject = useState('world wide web')
+//   const contents = useState([
+//     {
+//       id: 1,
+//       title: 'HTML',
+//       desc: 'HTML is for information',
+//     },
+//     { id: 2, title: 'CSS', desc: 'CSS is for design' },
+//     { id: 3, title: 'JS', desc: 'JS is for interactive' },
+//   ])
+//   return (
+//     <div className="App">
+//       <Subject title={title} sub={subject}></Subject>
+//       <TOC data={contents} />
+//       <Content />
+//     </div>
+//   )
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      contents: [
+        {
+          id: 1,
+          title: 'HTML',
+          desc: 'HTML is for information',
+        },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JS', desc: 'JS is for interactive' },
+      ],
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        />
+        <TOC data={this.state.contents} />
+      </div>
+    )
+  }
 }
 
 export default App
--- react_basic/src/components/TOC.js
+++ react_basic/src/components/TOC.js
@@ -1,19 +1,43 @@
-function TOC() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href="1.html">HTML</a>
+import { Component, useState } from 'react'
+
+// function TOC(props) {
+//   const [lists, setLists] = useState([])
+//   let data = props.data
+//   for (let i = 0; i < data.length; i++) {
+//     let temp = []
+//     temp.push(
+//       <li>
+//         <a href={`/content/` + data[i].id}>{data[i].title}</a>
+//       </li>
+//     )
+//     setLists(...lists, temp)
+//   }
+//   return (
+//     <nav>
+//       <ul>{lists}</ul>
+//     </nav>
+//   )
+// }
+
+class TOC extends Component {
+  render() {
+    var lists = []
+    var data = this.props.data
+    var i = 0
+    while (i < data.length) {
+      lists.push(
+        <li key={i}>
+          <a href={'/content/' + data[i].id}>{data[i].title}</a>
         </li>
-        <li>
-          <a href="2.html">CSS</a>
-        </li>
-        <li>
-          <a href="3.html">JS</a>
-        </li>
-      </ul>
-    </nav>
-  )
+      )
+      i++
+    }
+    return (
+      <nav>
+        <ul>{lists}</ul>
+      </nav>
+    )
+  }
 }
 
 export default TOC
