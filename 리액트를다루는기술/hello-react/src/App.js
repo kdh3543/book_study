0c3d9aa647d2d1510c9ff111b8035b522096e39b
김동현 <rlaehdgus3543@naver.com>
2023-06-08 15:22:15 +0900

    6/8 study

--- 리액트를다루는기술/hello-react/src/App.js
+++ 리액트를다루는기술/hello-react/src/App.js
@@ -1,17 +1,23 @@
 import { Component } from 'react';
 import Counter from './Counter';
 import Say from './Say';
-class App extends Component {
-  render() {
-    const name = 'react';
-    return (
-      <>
-        <Say />
-        <Counter />
-      </>
-    );
-  }
-}
+import EventPractice from './EventPractice';
+
+const App = () => {
+  return <EventPractice />;
+};
+
+// class App extends Component {
+//   render() {
+//     const name = 'react';
+//     return (
+//       <>
+//         <Say />
+//         <Counter />
+//       </>
+//     );
+//   }
+// }
 
 // function App() {
 //   const name = 'react';
