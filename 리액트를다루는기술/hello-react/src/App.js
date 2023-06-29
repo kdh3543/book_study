a4e4d7a7c7028ffc27b1b85805b2c3f2681f6178
김동현 <rlaehdgus3543@naver.com>
2023-06-29 16:21:24 +0900

    6/29 study

--- 리액트를다루는기술/hello-react/src/App.js
+++ 리액트를다루는기술/hello-react/src/App.js
@@ -5,16 +5,35 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+        {/* <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
         <button onClick={() => this.ScrollBox.scrollToBottom()}>
           맨 밑으로
         </button>
-        <IterationSample />
+        <IterationSample /> */}
       </div>
     );
   }
